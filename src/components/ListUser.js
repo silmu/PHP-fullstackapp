83d4da06764871f08e3fd85b2327072dafe40d5b
silmu <svetlana.raitina@gmail.com>
2022-04-20 19:45:44 +0300

    Fixed Access-Control error

--- src/components/ListUser.js
+++ src/components/ListUser.js
@@ -10,7 +10,7 @@ export function ListUser() {
   }, []);
 
   function getUsers() {
-    axios.get('http://localhost:8005/api').then(function(response) {
+    axios.get('http://localhost:8005/api/').then(function(response) {
       console.log(response.data);
       setUsers(response.data);
     });
